80c1af7bb7f7b816aeaf5ee4b7fcbab499435003
w3nch <133785769+w3nch@users.noreply.github.com>
2025-07-29 16:35:03 +0530

    Initial commit

---
+++ JS/Learning/test.js
@@ -0,0 +1,44 @@
+// class Hackers{
+//     constructor(name, level , HTB){
+//         this.name = name;
+//         this.level = level;
+//         this.HTB = HTB;
+//     }
+//     Hacker_full_detail(){
+//         return `Name: ${this.name} \nLevel: ${this.level} \nHTB: ${this.HTB}`;
+//     }
+//     Hacker_name(){
+//         return `Name: ${this.name}`;
+//     }
+//     Hacker_level(){
+//         return `Level: ${this.level}`;
+//     }
+//     Hacker_HTB(){
+//         return `HTB: ${this.HTB}`;
+//     }
+// }
+
+// const hacker1 = new Hackers("wrench", "Intermediate", "wr3nch");
+// console.log(hacker1.Hacker_full_detail());
+
+
+// class User {
+//     constructor(name, age, email) {
+//         this.name = name;
+//         this.age = age;
+//         this.email = email;
+//     }
+//     userDetails() {
+//         return `Name: ${this.name}, Age: ${this.age}, Email: ${this.email}`;
+//     }
+//    static create_Id() {
+//         return `${this.name}_${this.age}_${this.email}`;
+//     }
+// };
+
+// const user1 = new User("hacker", 30, "hacker@hacking.com");
+// console.log(user1.userDetails());
+// console.log(user1.create_Id());
+
+
+objec
